60adb7169b48da36c848dd20f2279f8e8aa889e7
Gerald Versluis <github@geraldversluis.nl>
2021-08-27 16:30:26 +0200

    Computer says no

---
+++ MauiApp13/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using Microsoft.Maui.Hosting;
+
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace MauiApp13
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureMauiHandlers(handlers =>
+                {
+                    handlers.AddCompatibilityRenderers(typeof(Xamarin.CommunityToolkit.UI.Views.SemanticOrderViewRenderer).Assembly);
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            return builder.Build();
+        }
+    }
+}
